58bc7276dcb96d3f862c839fc4ccdee598bd5d52
Chris Pettyjohn <chrismmpettyjohn>
2021-11-25 22:30:02 -0600

    Add design for rooms to rp admin panel

--- packages/roleplay-theme/src/pages/admin-panel/rooms/ListRooms.tsx
+++ packages/roleplay-theme/src/pages/admin-panel/rooms/ListRooms.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Col} from 'reactstrap';
-import {setURL} from '@instinct-web/core';
 import {Card} from '../components/card/Card';
+import {setURL, Icon} from '@instinct-web/core';
 import {Jumbotron} from '../components/jumbotron/Jumbotron';
 import {AdminLayout} from '../components/admin-layout/AdminLayout';
 
@@ -16,7 +16,42 @@ export function ListRooms() {
       <div className="page-content">
         <div className="row">
           <Col xs={12}>
-            <Card header="Rooms">Coming Soon</Card>
+            <Card header="Rooms">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Abilities</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Bank of Bobba</td>
+                    <td style={{overflow: 'hidden'}}>
+                      <div className="badge badge-pill badge-dark  mr-2">
+                        Bank
+                      </div>
+                      <div className="badge badge-pill badge-dark">
+                        Taxi Enabled
+                      </div>
+                    </td>
+                    <td>
+                      <button className="btn btn-outline-primary mr-2">
+                        <Icon type="pencil" />
+                        Edit
+                      </button>
+                      <button className="btn btn-outline-danger">
+                        <Icon type="trash" />
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </Card>
           </Col>
         </div>
       </div>

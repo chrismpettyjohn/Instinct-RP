273d40225abaf0585b886a666279aa78ab697f0a
Chris Pettyjohn <chrismmpettyjohn>
2021-12-05 19:13:16 -0600

    implement hot rooms

--- packages/roleplay-theme/src/pages/home/home/hot-rooms/HotRooms.tsx
+++ packages/roleplay-theme/src/pages/home/home/hot-rooms/HotRooms.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import {Icon} from '@instinct-web/core';
+import {take} from 'lodash';
+import {Icon, useFetchPopularRooms} from '@instinct-web/core';
 import {Card} from '../../../../components/generic/card/Card';
 
 export function HotRoomsCard() {
+  const hotRooms = useFetchPopularRooms();
+
+  const topFiveRooms = hotRooms ? take(hotRooms, 5) : [];
+
   return (
     <Card header="Hot Rooms">
       <table className="table">
@@ -13,15 +18,23 @@ export function HotRoomsCard() {
             <th scope="col">Users</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Canal St</td>
-            <td>
-              <Icon type="users" />3
-            </td>
-          </tr>
-        </tbody>
+        {hotRooms ? (
+          <tbody>
+            {topFiveRooms.map(room => (
+              <tr key={`hot_room_${room.id}`}>
+                <th scope="row">{room.id}</th>
+                <td>{room.name}</td>
+                <td>
+                  <Icon type="users" /> {room.currentUsers}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        ) : (
+          <>
+            <Icon type="spinner fa-spin" /> Loading...
+          </>
+        )}
       </table>
     </Card>
   );
